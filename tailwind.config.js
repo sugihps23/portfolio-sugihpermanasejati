dc6e0c694d542aff3e732457840822d50192d927
sugihps23 <sugihpermana123@gmail.com>
2022-06-27 07:10:05 +0700

    kedua commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#ef4444',
+        secondary: '#475569',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
